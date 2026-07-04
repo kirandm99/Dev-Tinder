d9fbf555ebd67ba6c0a8bfd5aaa0043c0d1a9ce2
kiran <kirandm99@gmail.com>
2026-07-04 23:37:31 +0530

    added feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequstModel = require("../models/connectionRequest");
+const UserModel = require("../models/user");
 const userRouter = express.Router();
+const USER_SAFE_DATA = "firstName lastName emailId photoUrl about skills";
 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
@@ -9,10 +11,7 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     const receivedRequests = await ConnectionRequstModel.find({
       toUserId: userId,
       status: "interested",
-    }).populate(
-      "fromUserId",
-      "firstName lastName emailId photoUrl about skills",
-    );
+    }).populate("fromUserId", USER_SAFE_DATA);
 
     res.status(200).json({
       message: "Received connection requests fetched successfully",
@@ -35,10 +34,7 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         { fromUserId: loggedUser, status: "accepted" },
         { toUserId: loggedUser, status: "accepted" },
       ],
-    }).populate(
-      "fromUserId toUserId",
-      "firstName lastName emailId photoUrl about skills",
-    );
+    }).populate("fromUserId toUserId", USER_SAFE_DATA);
 
     const data = connections.map((connection) => {
       if (connection.fromUserId._id.toString() === loggedUser.toString()) {
@@ -54,6 +50,42 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
   } catch (error) {
     res.status(500).json({
       message: "Error while fetching connections " + error.message,
+    });
+  }
+});
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const loggedUser = req.user._id;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
+    const connections = await ConnectionRequstModel.find({
+      $or: [{ fromUserId: loggedUser }, { toUserId: loggedUser }],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connections.forEach((connection) => {
+      hideUsersFromFeed.add(connection.toUserId.toString());
+      hideUsersFromFeed.add(connection.fromUserId.toString());
+    });
+
+    const feedUsers = await UserModel.find({
+      _id: { $ne: loggedUser, $nin: Array.from(hideUsersFromFeed) },
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.status(200).json({
+      message: "Feed fetched successfully",
+      data: feedUsers,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error while fetching feed " + error.message,
     });
   }
 });
